112062c4d181973d1f1a2caae71e3cba83f2a042
Marco Melletti <melletti.marco@gmail.com>
2014-11-20 11:57:09 +0100

    uninstall fix

--- uninstall.sh
+++ uninstall.sh
@@ -34,7 +34,7 @@ then
   rmdir --ignore-fail-on-non-empty `dirname "$TESTD"`
 fi
 
-rm -f "$BIND"/uarm "$BIND"/elf2uarm
+rm -f "$BIND"/uarm "$BIND"/elf2uarm "$BIND"/uarm-mkdev
 
 cd default
 for i in *
