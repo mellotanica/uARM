72382c5f668f77a617b69ae830dfd05596a95fba
Enrico Martelli <mrcnrt95e30@gmail.com>
2016-01-11 17:48:13 +0100

    Fixed installer

--- install.sh
+++ install.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 
 PREF="/usr/local"
-
 ICONSD=$PREF"/lib/uarm/icons"
-mkdir -p "$ICONSD"
-cp icons/* "$ICONSD"
-
 INCLUDED=$PREF"/include/uarm"
 TESTD=$PREF"/share/doc/uarm/examples"
 LDSCRIPTSD=$INCLUDED"/ldscripts"
 DEFAULTD="/etc/default"
 BIND=$PREF"/bin"
 
+mkdir -p "$ICONSD"
 mkdir -p "$INCLUDED"
 mkdir -p "$TESTD"
 mkdir -p "$LDSCRIPTSD"
 
+cp icons/* "$ICONSD"
 cp ldscript/* "$LDSCRIPTSD"
 cp test/* "$TESTD"
 cp facilities/* "$INCLUDED"
-
-if test $OS == "Linux"; then
-	cp uarm elf2uarm uarm-mkdev "$BIND"
-else
-	cp -r uarm.app /applications
-	cp elf2uarm "$BIND"
-fi
-
+cp elf2uarm "$BIND"
 cp default/* "$DEFAULTD"
