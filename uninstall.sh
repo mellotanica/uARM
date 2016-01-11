10a9558e1830d0641666d9c777289d45f4c183ab
Enrico Martelli <mrcnrt95e30@gmail.com>
2016-01-11 18:47:15 +0100

    Updated uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-if [ `id -u` != "0" ]; then
-	echo "You must be root to run this script" 1>&2
-	exit 1
-fi
-
-PREF="/usr"
-
-if [ "$1" == "-d" ]; then
-  PREF="$2"
-fi
-
-ICONSD=$PREF"/lib/uarm/icons"
+PREF="/usr/local"
 INCLUDED=$PREF"/include/uarm"
 TESTD=$PREF"/share/doc/uarm/examples"
 LDSCRIPTSD=$INCLUDED"/ldscripts"
@@ -19,27 +8,12 @@ DEFAULTD="/etc/default"
 BIND=$PREF"/bin"
 
 rm -rf "$ICONSD"
-if [ -d `dirname "$ICONSD"` ]
-then
-  rmdir --ignore-fail-on-non-empty `dirname "$ICONSD"`
-fi
 rm -rf "$INCLUDED"
-if [ -d `dirname "$INCLUDED"` ]
-then
-  rmdir --ignore-fail-on-non-empty `dirname "$INCLUDED"`
-fi
 rm -rf "$TESTD"
-if [ -d `dirname "$TESTD"` ]
-then
-  rmdir --ignore-fail-on-non-empty `dirname "$TESTD"`
-fi
-
-rm -f "$BIND"/uarm "$BIND"/elf2uarm "$BIND"/uarm-mkdev
+rm -f "$BIND"/elf2uarm
 
 cd default
 for i in *
 do
   rm -f "$DEFAULTD"/$i
 done
-
-exit 0
