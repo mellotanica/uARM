33d9e03d83214c305776503987a6f15022c5571c
Marco Melletti <mellotanica@hotmail.it>
2016-06-14 12:52:13 +0200

    enhanced tlb viewer accessibility

--- qarm/tlb_model.h
+++ qarm/tlb_model.h
@@ -51,10 +51,15 @@ public:
 
     QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
     bool setData(const QModelIndex& index, const QVariant& value, int role = Qt::EditRole);
+    QString getEmptyDetails() const;
+
+signals:
+    void entrySelected(QString details);
 
 public slots:
     void onTLBChanged(unsigned int index);
     void onMachineReset();
+    void activate(QModelIndex index);
 
 private:
     static const char* const detailsTemplate;
