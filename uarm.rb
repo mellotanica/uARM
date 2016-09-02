d847e4e19ba3cb65f4e4d0e69f266d728997fe32
Enrico Martelli <mrcnrt95e30@gmail.com>
2016-09-02 13:24:28 +0200

    Updated brew cask formula

--- uarm.rb
+++ uarm.rb
@@ -2,7 +2,7 @@ cask 'uarm' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/MartelliEnrico/uARM/archive/master.zip'
+  url 'https://github.com/MartelliEnrico/uARM/archive/master.tar.gz'
   name 'uARM'
   homepage 'http://mellotanica.github.io/uARM/'
   license :oss
@@ -22,7 +22,7 @@ cask 'uarm' do
   caveats do
     files_in_usr_local
 
-    print "You need to install an ARM cross-compiler to build ARM executable."
+    print "You need to install an ARM cross-compiler to build ARM executable.\n"
     print "You can find it at this link: https://launchpad.net/gcc-arm-embedded/+download"
   end
 end
